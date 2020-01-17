fb72e359b58faffe3de1ae65161f037844d34712
Michal Nowak <michalkacpernowak@gmail.com>
2020-01-17 21:10:04 +0100

    Break lines, Main update

--- printing.py
+++ printing.py
@@ -4,11 +4,13 @@ import os
 
 def count_games_print(file_name):
     games_count = reports.count_games(file_name)
-    print(f'How many games are in the file?     {games_count}')
+    print('How many games are in the file?   \n')
+    print(games_count)
 
 def decide_print(file_name):
     year = int(input("What year you are looking for? "))
     is_year_in_file = reports.decide(file_name, year)
+    print('')   #blank line
     if is_year_in_file == True:
         print(f"Game from Year {year} is in file")
     else:
@@ -19,14 +21,15 @@ def get_latest_print(file_name):
     print(f"Title of the latest game is: {latest_game_title}")
 
 def count_by_genre(file_name):
-    searched_genre = input("What genre are you looking for? ")
-    count_of_searched_genre = reports.count_by_genre(file_name, searched_genre)
+    genre = input("What genre are you looking for? ")
+    count_of_searched_genre = reports.count_by_genre(file_name, genre)
+    print('')   #blank line
     if count_of_searched_genre == 1:
-        print(f"Searched genre came up {count_of_searched_genre} time")
+        print(f"Searched genre came up {count_of_searched_genre} time.")
     if count_of_searched_genre == 0:
         print(f"Searched genre is not in a file")
     else:
-        print(f"Searched genre came up {count_of_searched_genre} times")
+        print(f"Searched genre came up {count_of_searched_genre} times.")
 
 def get_line_number_by_title_print(file_name):
     title = input("Insert title: ")
@@ -49,7 +52,8 @@ def main():
                 '6': sort_abc_print}
     file_name = input("Please enter name of the file: ")
     while True:
-        print(""""
+        try:
+            print(""""
                 Choose option:
                 (1) Count Games
                 (2) Decide
@@ -59,15 +63,17 @@ def main():
                 (6) Sort alphabetically
                 (0) Quit
                 """)
-        option = input("Please enter a number: ")
-        os.system('clear')
-        if option in dict_menu.keys():
-            dict_menu[option](file_name)
-        elif option == '0':
-            break
-        else:
-            raise KeyError("There is no such option.")
-        input("Enter '0' to go back to menu: ")
+            option = input("Please enter a number: ")
+            os.system('clear')
+            if option in dict_menu.keys():
+                dict_menu[option](file_name)
+            elif option == '0':
+                break
+            else:
+                raise KeyError
+        except KeyError:
+            print("There is no such option.")
+        input("\nEnter '0' to go back to menu: ")
 
 
 main()
